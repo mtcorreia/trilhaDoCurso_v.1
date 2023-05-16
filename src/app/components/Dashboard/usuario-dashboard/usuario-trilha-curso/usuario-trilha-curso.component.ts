64dac8d50fa2705764efa64b187ef3009c00e23a
Matheus Correia <mtcorreia99@hotmail.com>
2023-05-16 14:06:42 -0300

    mapaDoCurso_v1.0
    Projeto Integrador III

--- src/app/components/Dashboard/usuario-dashboard/usuario-trilha-curso/usuario-trilha-curso.component.ts
+++ src/app/components/Dashboard/usuario-dashboard/usuario-trilha-curso/usuario-trilha-curso.component.ts
@@ -19,13 +19,23 @@ export class UsuarioTrilhaCursoComponent implements OnInit {
   loading: boolean = true;
   senacCoinMovimentacoes : SenacCoinMovimentacao[]; 
   idUsuarioLogado : string;
+  
   unidadesCurriculares : UnidadeCurricular[];
+  unidadesCurricularesM1 : UnidadeCurricular[];
+  unidadesCurricularesM2 : UnidadeCurricular[];
+  unidadesCurricularesM3 : UnidadeCurricular[];
+  unidadesCurricularesM4 : UnidadeCurricular[];
+  unidadesCurricularesM5 : UnidadeCurricular[];
 
   constructor(private senacCoinService : SenacCoinService, private senacCoinMovimentacao : SenacCoinMovimentacaoService, private unidadeCurricularService : UnidadeCurricularService, private errorService : ErrorService) { }
 
   ngOnInit(): void {
 
-    this.ObterUnidadesCurriculares();
+    this.ObterUnidadesCurricularesM1();
+    this.ObterUnidadesCurricularesM2();
+    this.ObterUnidadesCurricularesM3();
+    this.ObterUnidadesCurricularesM4();
+    this.ObterUnidadesCurricularesM5();
 
     this.idUsuarioLogado = localStorage.getItem("UsuarioId")!;
 
@@ -40,7 +50,9 @@ export class UsuarioTrilhaCursoComponent implements OnInit {
     }); 
 
   }
-
+  
+  
+  // TODAS UNIDADES CURRICULARES
   ObterUnidadesCurriculares = () => {
     this.unidadeCurricularService.ObterTodasUnidadesCurriculares().pipe(
       delay(500),
@@ -54,6 +66,72 @@ export class UsuarioTrilhaCursoComponent implements OnInit {
     });
   }
 
+  // MÓDULO 1
+  ObterUnidadesCurricularesM1 = () => {
+    this.unidadeCurricularService.ObterUnidadeCurricularPeloModuloId(1).pipe(
+      delay(500),
+      catchError((error) => {
+        this.errorService.onError('Erro ao carregar unidades curriculares.');
+        return of([]);
+      })
+    ).subscribe((resultadoUnidadesCurriculares) => {
+      this.unidadesCurricularesM1 = resultadoUnidadesCurriculares;
+      console.log(this.unidadesCurricularesM1);
+    });
+  }
+      // MÓDULO 2
+  ObterUnidadesCurricularesM2 = () => {
+    this.unidadeCurricularService.ObterUnidadeCurricularPeloModuloId(2).pipe(
+      delay(500),
+      catchError((error) => {
+        this.errorService.onError('Erro ao carregar unidades curriculares.');
+        return of([]);
+      })
+    ).subscribe((resultadoUnidadesCurriculares) => {
+      this.unidadesCurricularesM2 = resultadoUnidadesCurriculares;
+      console.log(this.unidadesCurricularesM2);
+    });
+  }
 
-  
+  // MÓDULO 3
+  ObterUnidadesCurricularesM3 = () => {
+    this.unidadeCurricularService.ObterUnidadeCurricularPeloModuloId(3).pipe(
+      delay(500),
+      catchError((error) => {
+        this.errorService.onError('Erro ao carregar unidades curriculares.');
+        return of([]);
+      })
+    ).subscribe((resultadoUnidadesCurriculares) => {
+      this.unidadesCurricularesM3 = resultadoUnidadesCurriculares;
+      console.log(this.unidadesCurricularesM3);
+    });
+  }  
+
+  // MÓDULO 4
+  ObterUnidadesCurricularesM4 = () => {
+    this.unidadeCurricularService.ObterUnidadeCurricularPeloModuloId(4).pipe(
+      delay(500),
+      catchError((error) => {
+        this.errorService.onError('Erro ao carregar unidades curriculares.');
+        return of([]);
+      })
+    ).subscribe((resultadoUnidadesCurriculares) => {
+      this.unidadesCurricularesM4 = resultadoUnidadesCurriculares;
+      console.log(this.unidadesCurricularesM4);
+    });
+  }  
+    // MÓDULO 5
+    ObterUnidadesCurricularesM5 = () => {
+      this.unidadeCurricularService.ObterUnidadeCurricularPeloModuloId(5).pipe(
+        delay(500),
+        catchError((error) => {
+          this.errorService.onError('Erro ao carregar unidades curriculares.');
+          return of([]);
+        })
+      ).subscribe((resultadoUnidadesCurriculares) => {
+        this.unidadesCurricularesM5 = resultadoUnidadesCurriculares;
+        console.log(this.unidadesCurricularesM5);
+      });
+    }
+
 }
